01aba632904ff4587f1d02a765ad2f30800cc617
Konrad Tjaden <konrad.tjaden@computacenter.com>
2021-04-21 10:57:32 +0200

    corrected indentation & added some commentary

--- src/esp8266_hc-sr04_wifi_mqtt-new.ino
+++ src/esp8266_hc-sr04_wifi_mqtt-new.ino
@@ -14,22 +14,22 @@
 #define ULTRASONIC_ECHO_PIN     4   // pin ECHO to D2
 
 // tank specific: TODO - add water level %
-int tank_height = 200; // maximum height of tank in cm
-int tank_liters = 5000; // maximum height of tank in cm
-float volume; // volume in cubic cm
-float distance; // height of water level in cm
-int radius = 75; // radius of tank in cm
-int cap; // capacity in liters
-int actual_height; // actual height in cm
-int liters_per_cm = 25; // for the roto 5kl it is 5000(max liters)/200(height)=25L
-int liters_per_wash = 80; // used to calculated wasing maching cycles remaining from tank
-int cycles_remaining;
-int percentage; // % of of tank_liters
+int tank_height = 200;    // maximum height of tank in cm
+int tank_liters = 5000;   // maximum height of tank in cm
+float volume;             // volume in cubic cm
+float distance;           // height of water level in cm
+int radius = 75;          // radius of tank in cm
+int cap;                  // capacity in liters
+int actual_height;        // actual height in cm
+int liters_per_cm = 25;   // for the roto 5kl it is 5000(max liters)/200(height)=25L
+int liters_per_wash = 80; // used to calculated washing maching cycles remaining from tank
+int cycles_remaining;     // used to calculate how many washing machine wash cycles the tank can service
+int percentage; 		  // % of of tank_liters
 
 // user config: TODO
-const char* wifi_ssid = "SSID";  // SSID
-const char* wifi_password = "Password";                // WIFI
-const unsigned int writeInterval = 2500;               // write interval (in ms)
+const char* wifi_ssid = "SSID";          // SSID
+const char* wifi_password = "Password";  // WIFI_Pass
+const unsigned int writeInterval = 2500; // write interval (in ms)
 
 // Wifi
 WiFiClient wclient;
@@ -71,66 +71,68 @@ void setup() {
 void loop() {
 
     // wait for WiFi connection
-  if (WiFi.status() == WL_CONNECTED){
+	if (WiFi.status() == WL_CONNECTED){
 
-        long duration, distance;
-        digitalWrite(ULTRASONIC_TRIG_PIN, LOW);  
-        delayMicroseconds(2); 
-        
-        digitalWrite(ULTRASONIC_TRIG_PIN, HIGH);
-        delayMicroseconds(10); 
-        
-        digitalWrite(ULTRASONIC_TRIG_PIN, LOW);
-        duration = pulseIn(ULTRASONIC_ECHO_PIN, HIGH);
-        distance = (duration/2) / 29.1;
-    String mqtt_dist = String(distance); // using a long
+		long duration, distance;
+		digitalWrite(ULTRASONIC_TRIG_PIN, LOW);  
+		delayMicroseconds(2); 
+		
+		digitalWrite(ULTRASONIC_TRIG_PIN, HIGH);
+		delayMicroseconds(10); 
+		
+		digitalWrite(ULTRASONIC_TRIG_PIN, LOW);
+		duration = pulseIn(ULTRASONIC_ECHO_PIN, HIGH);
+		distance = (duration/2) / 29.1;
+		String mqtt_dist = String(distance); // using a long
 
-    // echo to console
-        Serial.print("Ultrasonic Distance: ");
-        Serial.print(distance);
-        Serial.println(" cm");
-        
-    // calculate the capacity in liters
-    // volume calculation were off - moved to liters per cm instead
-    actual_height = tank_height - distance;
-      // volume = ((3.14*(radius*radius))*(actual_height)); // formula to calculate volume in cubic cm
-      //cap = volume/1000; // final capacity in liters
-    cap = (actual_height*liters_per_cm);
-    String mqtt_cap = String(cap); // using a long
+		// echo sensor data to console
+		Serial.print("Ultrasonic Distance: ");
+		Serial.print(distance);
+		Serial.println(" cm");
+			
+		// calculate the capacity in liters
+		// volume calculation were off - moved to liters per cm instead
+		actual_height = tank_height - distance;
+		// volume = ((3.14*(radius*radius))*(actual_height)); // formula to calculate volume in cubic cm
+		//cap = volume/1000; // final capacity in liters
+		cap = (actual_height*liters_per_cm);
+		String mqtt_cap = String(cap); // using a long
 
-    // calculate the washing machine cycles remaining
-    cycles_remaining = (cap/liters_per_wash);
-    String mqtt_cycles_remaining = String(cycles_remaining); // using a long
+		// calculate the washing machine cycles remaining
+		cycles_remaining = (cap/liters_per_wash);
+		String mqtt_cycles_remaining = String(cycles_remaining); // using a long
 
-    // calculate % full
-    percentage = (cap/tank_liters*100);
-    String mqtt_percentage = String(percentage); // using a long
-    
-        Serial.print("Capacity: ");
-        Serial.print(cap);
-        Serial.println(" liters in tank");
-    Serial.print("Pct free: ");
-    Serial.println(percentage);
-    Serial.print("Wash Cyckes remaining: ");
-    Serial.println(cycles_remaining);
+		// calculate % full
+		percentage = (cap/tank_liters*100);
+		String mqtt_percentage = String(percentage); // using a long
+		
+		// echo out calculated values
+		Serial.print("Capacity: ");
+		Serial.print(cap);
+		Serial.println(" liters in tank");
+		Serial.print("Pct free: ");
+		Serial.println(percentage);
+		Serial.print("Wash Cyckes remaining: ");
+		Serial.println(cycles_remaining);
 
-    if (!client.connected()) {
-      if (client.connect("esp8266Client")) {
-        client.publish("rototank/sensor/hcrs04",mqtt_dist);
-        client.publish("rototank/sensor/liters",mqtt_cap);
-        client.publish("rototank/sensor/PctFree",mqtt_percentage);
-        client.publish("rototank/sensor/WashCyclesRemaining",mqtt_cycles_remaining);
-        // client.subscribe("inTopic"); // Future upgrade
-      }
-    }else{
-      if (client.connect("esp8266Client")) {
-        client.publish("rototank/sensor/hcrs04",mqtt_dist);
-        client.publish("rototank/sensor/liters",mqtt_cap);
-        client.publish("rototank/sensor/PctFree",mqtt_percentage);
-        client.publish("rototank/sensor/WashCyclesRemaining",mqtt_cycles_remaining);
-        // client.subscribe("inTopic"); // Future upgrade
-      }     
-    }
+		// publish mqtt topics
+		if (!client.connected()) {
+			if (client.connect("esp8266Client")) {
+				client.publish("rototank/sensor/hcrs04",mqtt_dist);
+				client.publish("rototank/sensor/liters",mqtt_cap);
+				client.publish("rototank/sensor/PctFree",mqtt_percentage);
+				client.publish("rototank/sensor/WashCyclesRemaining",mqtt_cycles_remaining);
+				// client.subscribe("inTopic"); // Future upgrade
+			}
+		}else{
+			if (client.connect("esp8266Client")) {
+				client.publish("rototank/sensor/hcrs04",mqtt_dist);
+				client.publish("rototank/sensor/liters",mqtt_cap);
+				client.publish("rototank/sensor/PctFree",mqtt_percentage);
+				client.publish("rototank/sensor/WashCyclesRemaining",mqtt_cycles_remaining);
+				// client.subscribe("inTopic"); // Future upgrade
+			}     
+		}
     }
 
     delay(writeInterval);
